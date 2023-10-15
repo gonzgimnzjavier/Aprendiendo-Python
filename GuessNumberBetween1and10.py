56987120e666a39224eae2ccbf686ec351df8413
Javier González <javigongimenez@gmail.com>
2023-10-15 11:16:16 +0200

    Big improvements

--- GuessNumberBetween1and10.py
+++ GuessNumberBetween1and10.py
@@ -1,9 +1,25 @@
-# En este programa se genera un numero random entre 1 y 10 y el usuario tiene que adivinarlo
+# Este programa genera un numero aleatorio entre 1 y 10 y el usuario debe adivinarlo
 import random
-number = random.randint(1, 10)
-guess = int(input("Adivina el numero: "))
-if guess == number:
-    print("Ganaste")
-else:
-    print("Perdiste")
-    print("El numero era:", number)
+# while True el bucle se repite mientras se cumpla la condición
+while True:
+    number = random.randint(1, 10)
+    guess = 0
+
+    while guess != number:
+        guess = int(input("Adivina el número entre 1 y 10: "))
+        if guess < 1 or guess > 10:
+            print("El número debe estar entre 1 y 10.")
+            if guess == 666:
+                print("Sin embargo, has elegido el número de la bestia.")
+            elif guess == -666:
+                print("Sin embargo, has elegido el número de la bestia invertido eso es doblemente maligno.")
+        elif guess > number:
+            print("El número es menor.")
+        elif guess < number:
+            print("El número es mayor.")
+
+    print("¡Ganaste!")
+
+    play_again = input("¿Quieres jugar de nuevo? (s/n): ")
+    if play_again.lower() != "s":
+        break
