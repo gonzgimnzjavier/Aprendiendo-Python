9310e616e2b07e566ec5425aeb81fed5cb85b7cb
Javier González <javigongimenez@gmail.com>
2023-10-15 17:21:34 +0200

    Added several changes to my Jojo quiz

--- quizJojo.py
+++ quizJojo.py
@@ -1,4 +1,6 @@
 # Programa en Python quiz sobre la serie manga de Jojo
+from _ast import While
+
 puntuacion = 0
 titulo = "Bienvenido al Quiz sobre la serie manga de Jojo's Bizarre Adventure"
 print("-" * len(titulo))
@@ -6,40 +8,49 @@ print(titulo)
 print("-" * len(titulo))
 input("Presiona enter para comenzar")
 print("")
-pregunta = input("¿En la parte 4, Diamond is Unbreakable, ¿cuál es el nombre del arco argumental centrado en el antagonista Yoshikage Kira?"
-    "\na) Sheer Heart Attack\nb) Killer Queen\nc) Bites the Dust\n").lower()
-if pregunta == "a":
-    puntuacion += 1
-elif pregunta == "b":
-    puntuacion += 2
-elif pregunta == "c":
-    puntuacion += 3
-else:
-    print("Las respuestas son a, b o c")
-    exit()
-pregunta = input("¿En qué año se publicó por primera vez el manga de Jojo's Bizarre Adventure?\n"
-    "a) 1980\nb) 1987\nc) 1989").lower()
-if pregunta == "a":
-    puntuacion += 1
-elif pregunta == "b":
-    puntuacion += 3
-elif pregunta == "c":
-    puntuacion += 2
-else:
-    print("Las respuestas son a, b, c o d")
-    exit()
 
-pregunta = input("¿Cual es el nombre del stand de Enya?\n"
-    "a) Strength\nb) Wheel of Fortune\n""c) Justice\n").lower()
+# Se define una lista con las opciones validas
+opcionesValidas = ["a", "b", "c"]
+while True:
+    pregunta = input("¿En la parte 4, Diamond is Unbreakable, ¿cuál es el nombre del arco argumental centrado en el antagonista Yoshikage Kira?"
+        "\na) Sheer Heart Attack\nb) Killer Queen\nc) Bites the Dust\n").lower()
+    if pregunta in opcionesValidas:
+        break
+    else:
+        print("Las respuestas son a, b, o c")
 if pregunta == "a":
     puntuacion += 1
 elif pregunta == "b":
     puntuacion += 2
 elif pregunta == "c":
     puntuacion += 3
-else:
-    print("Las respuestas son a, b, c o d")
-    exit()
+
+while True:
+    pregunta = input("¿En qué año se publicó por primera vez el manga de Jojo's Bizarre Adventure?\n"
+        "a) 1980\nb) 1987\nc) 1989").lower()
+    if pregunta in opcionesValidas:
+        break
+    else:
+        print("Las respuestas son a, b, o c")
+if pregunta == "a":
+    puntuacion += 1
+elif pregunta == "b":
+    puntuacion += 3
+elif pregunta == "c":
+    puntuacion += 2
+while True:
+    pregunta = input("¿Cual es el nombre del stand de Enya?\n"
+        "a) Strength\nb) Wheel of Fortune\n""c) Justice\n").lower()
+    if pregunta in opcionesValidas:
+        break
+    else:
+        print("Las respuestas son a, b, o c")
+if pregunta == "a":
+    puntuacion += 1
+elif pregunta == "b":
+    puntuacion += 2
+elif pregunta == "c":
+    puntuacion += 3
 
 if puntuacion == 9:
     print("Has acertado todas: Di Molto!")
